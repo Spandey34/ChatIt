896d3e0747823e9622fd3b1e12a6dd89907b1aff
Spandey34 <shubhamkumarpandey1844@gmail.com>
2025-08-15 20:32:07 +0530

    production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,8 +17,10 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
+    origin: process.env.NODE_ENV === "production"
+      ? undefined
+      : "http://localhost:5173",
+    credentials: true,
   })
 );
 
