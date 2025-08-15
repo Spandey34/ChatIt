6f9e1ce5248267cc236b133862e3bd031e1ca58c
Spandey34 <shubhamkumarpandey1844@gmail.com>
2025-08-16 01:08:29 +0530

    ..

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.BACKEND_URL ? `${import.meta.env.BACKEND_URL}/api` : `${import.meta.env.MODE}` === "development" ? "http://localhost:5001/api" : "/api";
-console.log(BASE_URL);
+const BASE_URL = import.meta.env.BACKEND_URL ? `${import.meta.env.BACKEND_URL}/api` : (`${import.meta.env.MODE}` === "development" ? "http://localhost:5001/api" : "/api");
+console.log(import.meta.env.BACKEND_URL);
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
