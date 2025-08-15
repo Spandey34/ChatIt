2d4be214e0cd6fa3c8c327d168dd57816d384910
Spandey34 <shubhamkumarpandey1844@gmail.com>
2025-08-16 01:37:42 +0530

    ..

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_STREAM_API_KEY ? `${import.meta.env.VITE_STREAM_API_KEY}/api` : (`${import.meta.env.MODE}` === "development" ? "http://localhost:5001/api" : "/api");
+const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL ? `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api` : (`${import.meta.env.MODE}` === "development" ? "http://localhost:5001/api" : "/api");
 console.log(BASE_URL);
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
